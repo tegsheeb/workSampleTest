d880f8bb063a935f8c5b85015bb975289e4adfb8
Tegshee Baasan <tegshbayar@gmail.com>
2020-09-16 17:07:47 -0700

    Implement functionality to change color of the circle

--- 3.React/src/app.jsx
+++ 3.React/src/app.jsx
@@ -1,11 +1,61 @@
-import React from "react"
+import React, {Component} from "react"
+import "./style.scss"
 
-const App = () => {
-    return(
-        <div>
-            This is from App.jsx
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            inputColor: "",
+            currentColor: "black",
+            message: ''
+            }
+  
+      this.handleChange = this.handleChange.bind(this)
+      this.changeColor = this.changeColor.bind(this)
+    }
+  
+    handleChange(event) {
+      this.setState({inputColor: event.target.value})
+    }
+  
+    isColor(strColor){
+        var s = new Option().style;
+        s.color = strColor;
+        return s.color == strColor;
+    }
+    
+    changeColor() {
+        let inputColor = this.state.inputColor.toLowerCase()
+        if(this.isColor(inputColor)) {
+            this.setState({
+                currentColor : inputColor
+            })
+            this.setState({
+                inputColor : "",
+                message : "",
+            })
+        } else {
+            this.setState({
+                message : "Please enter a valid color"
+            })
+        }
+    }
+
+    render() {
+      return (
+        <div className="app">
+        <div className="circle" style={{backgroundColor: this.state.currentColor}}> </div>
+        <form >
+          <label>
+            Enter color of the circle:
+            <input type="text" value={this.state.inputColor} onChange={this.handleChange} />
+          </label>
+        </form>
+        <button onClick={this.changeColor}> Change color! </button>
+        <div> {this.state.message}</div>
         </div>
-    )
-}
+      );
+    }
+  }
 
 export default App
