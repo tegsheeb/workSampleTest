988fec82a46e3b6287fbac6338b9ff91712f0788
Tegshee Baasan <tegshbayar@gmail.com>
2020-10-05 11:59:13 -0700

    Implement go solution 1 and 2

--- 1.Go/workGo.go
+++ 1.Go/workGo.go
@@ -22,7 +22,7 @@ func exactTwice(people []Person) bool {
 }
 */
 
-
+/*
 // Time Complexity: O(n)
 func exactTwice(people []Person) bool {
 	ages := make(map[uint16]bool)
@@ -38,7 +38,54 @@ func exactTwice(people []Person) bool {
 	}
 	return false
 }
+*/
 
+/* exactTwice func with the following constraint: 
+	* Age is an integer with values that range from 0 to 50
+	* you may only use variables of numeric types (no array/slice/map)
+	* must have linear time complexity with just a single loop
+	* you may not use addition, subtraction, multiplication, division or modulo operators
+	* the function may not consist of more than 15 lines of code
+	* the function may not call other functions
+*/
+
+/*
+// First version: bitwise operation, single loop, O(n) time complexity with 12 lines
+func exactTwice(people []Person) bool {
+	original := people[0].age 
+	leftShift := people[0].age << 1
+	rightShift := people[0].age >> 1
+
+	for i := 1; i < len(people); i++{
+		original ^= people[i].age
+		leftShift ^= (people[i].age << 1)
+		rightShift ^= (people[i].age >> 1)
+	}
+	return (original & (leftShift ^ rightShift)) == 0
+}
+*/
+
+// Second version: bitwise operation, O(n) time complexity with 20 lines
+func exactTwice(people []Person) bool {
+	for age := 0; age <= 25; age++ {
+		twiceAge := age << 1
+		foundAge := false
+		foundTwiceAge := false
+		
+		for _, Person := range people {
+			if Person.age == age {
+				foundAge = true
+			} else if Person.age == twiceAge {
+				foundTwiceAge = true
+			}
+		}
+		
+		if foundAge && foundTwiceAge {
+			return true
+		}
+	}
+	return false
+}
 
 /*
 // Time Complexity: O(n^2)
@@ -54,7 +101,7 @@ func atLeastTwice(people []Person) bool {
 }
 */
 
-
+/*
 // Time Complexity: O(n)
 func atLeastTwice(people []Person) bool {
 	if len(people) == 0 {
@@ -76,23 +123,25 @@ func atLeastTwice(people []Person) bool {
 
 	return minAge*2 <= maxAge
 }
+*/
 
 func main() {
-	var peopleOne, peopleTwo, peopleThree []Person
-	person1 := Person{age: 5}
-	person2 := Person{age: 9}
-	person3 := Person{age: 11}
-	person4 := Person{age: 12}
-	person5 := Person{age: 10}
+	var peopleTwo /*,peopleThree */ []Person
+	person1 := Person{age: 9}
+	person2 := Person{age: 18}
+	person3 := Person{age: 6}
+	// person4 := Person{age: 12}
+	// person5 := Person{age: 25}
+	// person6 := Person{age: 17}
+	// person7 := Person{age: 50}
 
+	// peopleOne = append(peopleOne, person1, person2, person3, person4, person5, person6, person7)
+	peopleTwo = append(peopleTwo, person1, person2, person3)
 
-	peopleOne = append(peopleOne, person1, person2, person3, person4, person5)
-	peopleTwo = append(peopleTwo, person3, person4, person5)
-
-	fmt.Println(exactTwice(peopleOne))
+	// fmt.Println(exactTwice(peopleOne))
 	fmt.Println(exactTwice(peopleTwo))
-	fmt.Println(atLeastTwice(peopleOne))
-	fmt.Println(atLeastTwice(peopleTwo))
-	fmt.Println(atLeastTwice(peopleThree))
-	fmt.Println(atLeastTwice(peopleThree))
+	// fmt.Println(atLeastTwice(peopleOne))
+	// fmt.Println(atLeastTwice(peopleTwo))
+	// fmt.Println(atLeastTwice(peopleThree))
+	// fmt.Println(atLeastTwice(peopleThree))
 }
