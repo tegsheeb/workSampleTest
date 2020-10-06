5099c5f6379f241f84aa8649ebbdd8fe13befc37
Tegshee Baasan <tegshbayar@gmail.com>
2020-10-06 00:37:01 -0700

    Refactor comment

--- 2.MariaDB/queries.sql
+++ 2.MariaDB/queries.sql
@@ -8,16 +8,15 @@ SELECT u1.username, u1.first_name, u1.last_name, tweet_id, tweet, posted_date fr
         DESC LIMIT 30;
 
 /*
-    2.2) One minute later, to scroll down to next 30 tweets
-    tweet_id is unique within entire database, therefore, I have refactored
-    to include tweet_id into query result, so that I can retrieve back
-    tweet_id of the last tweet of the the 30 last quiried from the front-end. 
-    Also, there can be tweets posted at exact same momemt but one was already
-    queried and others are still not queried. Therefore, passing the posted_date of
-    the last tweet of the last 30 tweets already queried as an argument to when 
-    fetching from endpoint. 
-    When front-end fetches data from the endpoint, it will pass the last_tweet_id and 
-    the last_posted_date of last of 30 tweets' as arguments. 
+    2.2) One minute later to fetch next 30 tweets without duplicates, 
+    backend will receive the last_tweet_id and the last_posted_date of last of 30 tweets'
+    as arguments when front-end fetches data from the endpoint. 
+    I have refactored to include tweet_id into query as tweet_id is 
+    unique within entire database. In addition to that, there can be tweets 
+    posted at exact same momemt but some are already queried and 
+    others are still not queried to the previous 30 tweets. 
+    Therefore, passing the posted_date of the last tweet of the last 30 tweets 
+    already queried as an argument to when fetching from endpoint, as well. 
 */
 
 $last_tweet_id = `passed_in_from_front_end`
